2b932b5fd5424ac7b0a3382748381f0d38ae0107
webapis <60083568+webapis@users.noreply.github.com>
2022-11-09 08:51:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-import {Routes,Route,HashRouter} from 'react-router-dom'
+import {Routes,Route,HashRouter,Link} from 'react-router-dom'
 import Contacts from './pages/Contacts';
 import Home from './pages/Home';
 function App() {
   return <HashRouter>
+    <Link to="/contacts">Contacts</Link>
   <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='contacts' element={<Contacts/>}/>
